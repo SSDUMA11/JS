c77e6d52c50de436ced5a2a0dc2979e976ba4cc0
SSDUMA11 <dumasofia2@gmail.com>
2023-02-07 16:36:30 +0200

    tasks_three

--- app.js
+++ app.js
@@ -17,3 +17,12 @@ alert(`Your full name: ${userName} ${surName} ${yourAge} ${userAge}`)
 
 let userAdmin = confirm('Are you admin?');
 alert(`Your full name: ${userName} ${surName} ${yourAge} ${userAge}, admin status is: ${userAdmin}`);
+
+//============================================
+
+// console.log(Boolean (alert("5")));//false (alert нічого не повертає, пустий рядок стає 0, а 0- false )
+// console.log(true > false); // true (true=1, false=0, 1>0 , тому true)
+// console.log(true > 5); // false (true=1, 5=5, 1>5(невірно) , тому false)
+// console.log(true < "5");// true (true=1, "5"=5, 1<5(вірно) , тому true)
+// console.log(true > null); // true (true=1, null=0, 1>0(вірно) , тому true)
+
